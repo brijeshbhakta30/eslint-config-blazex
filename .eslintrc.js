535944a66599894c58fcf3f1af248b3ffd99c651
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2022-10-12 20:48:16 +0100

    feat: ✨ eslint configs for react and without react

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:sonarjs/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-config-blaze
+
+This package provides multiple .eslintrc config files an extensible shared config.
