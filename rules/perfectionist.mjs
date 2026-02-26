a4dd5b223e1818e609eaf5f7602d196f17e3b077
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2026-02-26 21:46:41 +0000

    fix: 🐛 vulnerabilities

--- rules/perfectionist.mjs
+++ rules/perfectionist.mjs
@@ -1,7 +1,7 @@
-import perfectionist from 'eslint-plugin-perfectionist';
+import { configs } from 'eslint-plugin-perfectionist';
 
 export default {
-  ...perfectionist.configs['recommended-natural'].rules,
+  ...configs['recommended-natural'].rules,
   'perfectionist/sort-modules': 0,
   'perfectionist/sort-object-types': 0,
   'perfectionist/sort-objects': 0,
